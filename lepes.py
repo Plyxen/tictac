e810c4bd0ddf8f8ba21a2a77ff5e90ab3b3abc00
SzudaTiborSzilveszter <szudatiborszilveszter@gmail.com>
2025-11-20 14:30:58 +0100

    Refactor lepes function to accept tabla parameter and update logic for move placement

--- lepes.py
+++ lepes.py
@@ -1,13 +1,29 @@
 import random
 import kiiras
 
-def lepes(meret):
-    gep_lepes=[]
+def lepes(meret, tabla):
     sor = random.randint(0, int(meret)-1)
-    gep_lepes.append(sor)
     oszlop = random.randint(0, int(meret)-1)
-    gep_lepes.append(oszlop)
-    return gep_lepes
+    tabla[sor][oszlop] = 'O'
+    
+    kiiras.palya(meret, tabla)
+    
+    return tabla
 
-"""eredmeny = lepes()
-print("A lépés:", eredmeny)"""
+def jatekos_lepes(meret, tabla):
+    input("Add meg a lépésed (pl. A1): ")
+    i=0
+    while i < 1:
+        lepes = input("Add meg a lépésed (pl. A1): ")
+        sor = ord(lepes[0].upper()) - 65
+        oszlop = int(lepes[1:]) - 1
+
+        if sor < 0 or sor >= meret or oszlop < 0 or oszlop >= meret:
+            print("Érvénytelen lépés. Próbáld újra.")
+        elif tabla[sor][oszlop] != ' ':
+            print("Ez a mező már foglalt. Próbáld újra.")
+        else:
+            tabla[sor][oszlop] = 'X'
+            i += 1
+    kiiras.palya(meret, tabla)
+    return tabla
