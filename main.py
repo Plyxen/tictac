e246f57cf9c9afc3b8dde6a19e594cecd9544d33
SzudaTiborSzilveszter <szudatiborszilveszter@gmail.com>
2025-11-20 13:54:33 +0100

    Implement board initialization and update main.py to define the tabla function

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 import kiiras
+import lepes
 
-kiiras.kiiras()
+def tabla(meret):
+    tabla = [] 
+    tabla_sor_index = 0
+    while tabla_sor_index < meret:
+        tabla_oszlop_index = 0
+        aktualis_sor = [] 
+        while tabla_oszlop_index < meret:
+            aktualis_sor.append(' ') 
+            tabla_oszlop_index = tabla_oszlop_index + 1
+        tabla.append(aktualis_sor)
+        tabla_sor_index = tabla_sor_index + 1
+
+    tabla[2][1] = 'X' 
+    tabla[0][0] = 'O'
+
+    return tabla
+    
+meret=kiiras.palya_meret()
+jatekos_tabla=tabla(meret)
+kiiras.palya(meret, jatekos_tabla)
+lepes.lepes(meret)
